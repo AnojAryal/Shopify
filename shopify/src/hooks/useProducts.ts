e61bef2a1b2791d6c701dfb5b39dd26c074df0e0
FraNzY <anoj1810@gmail.com>
2023-07-01 15:06:10 +0545

    Displaying size icons

--- shopify/src/hooks/useProducts.ts
+++ shopify/src/hooks/useProducts.ts
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
-interface Size {
+export interface Size {
   id : number,
   name : string,
   slug : string
