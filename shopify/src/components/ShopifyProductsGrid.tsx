bdf85037f424af8f95972fd784489b87441fca4f
FraNzY <anoj1810@gmail.com>
2023-07-11 17:20:01 +0545

    Refactor : Productgrid

--- shopify/src/components/ShopifyProductsGrid.tsx
+++ shopify/src/components/ShopifyProductsGrid.tsx
@@ -13,27 +13,26 @@ const ShopifyProductGrid = ({ shopQuery }: Props) => {
   const { data, error, isLoading } = useProducts(shopQuery);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+  if (error) return <Text>{error}</Text>;
+
   return (
-    <>
-      {error && <Text>{error}</Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        padding="10px"
-        spacing={6}
-      >
-        {isLoading &&
-          skeletons.map((skeleton) => (
-            <ProductCardContainer key={skeleton}>
-              <ProductCardSkeleton />
-            </ProductCardContainer>
-          ))}
-        {data.map((product) => (
-          <ProductCardContainer key={product.id}>
-            <ProductCard product={product} />
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+      padding="10px"
+      spacing={6}
+    >
+      {isLoading &&
+        skeletons.map((skeleton) => (
+          <ProductCardContainer key={skeleton}>
+            <ProductCardSkeleton />
           </ProductCardContainer>
         ))}
-      </SimpleGrid>
-    </>
+      {data.map((product) => (
+        <ProductCardContainer key={product.id}>
+          <ProductCard product={product} />
+        </ProductCardContainer>
+      ))}
+    </SimpleGrid>
   );
 };
 
